2d26f76d4faa3f2faeda861f3a4f38ce4e525503
casjora <casjora@gmail.com>
2024-04-27 15:34:02 -0600

    Core requirements for prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using Microsoft.VisualBasic;
 
 class Program
 {
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName (string userName)
+    {
+        Console.WriteLine("Please enter your name: ");
+        userName = Console.ReadLine();
+        return userName;
+    }
+    static int PromptNumber (int number)
+    {
+        Console.WriteLine("What's your favorite integer number? ");
+        string inputNumber = Console.ReadLine();
+        number = int.Parse(inputNumber);
+
+        return number;
+    }
+
+    static int SquareNumber  (int number)
+    {
+        int square = number*number;
+
+        return square;
+    }
+
+    static void DisplayResult (string userName,int square)
+    {
+        Console.WriteLine($"{userName}, the square of your number is {square}");
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = PromptUserName("");
+        int number=PromptNumber(0);
+        int square=SquareNumber(number);
+        DisplayResult(name,square);
+
     }
 }
