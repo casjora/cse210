dc90909612e2b32af0e95dbaf200248bf36a2da8
casjora <casjora@gmail.com>
2024-04-27 12:21:21 -0600

    Core requirements code for prep#1

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,39 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please enter your grade as an integer: ");
+        string inputGrade = Console.ReadLine();
+        int grade = int.Parse(inputGrade);
+        string letterGrade;
+
+        if (grade>=90)
+        {
+            letterGrade = "A";
+            Console.WriteLine($"Your letter grade is {letterGrade}");
+        }
+
+        else if (grade>=80)
+        {
+        letterGrade = "B";
+        Console.WriteLine($"Your letter grade is {letterGrade}");
+        }
+        else if (grade>=70)
+        {
+        letterGrade = "C";
+        Console.WriteLine($"Your letter grade is {letterGrade}");
+        }
+        else if (grade>=60)
+        {
+        letterGrade = "D";
+        Console.WriteLine($"Your letter grade is {letterGrade}");
+        }
+        else
+        {
+        letterGrade = "F";
+        Console.WriteLine($"Your letter grade is {letterGrade}");
+        }
+
+        
+
     }
 }
