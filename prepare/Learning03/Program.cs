016429185bd55265244644643a4c68ad2421a6f3
casjora <casjora@gmail.com>
2024-05-08 22:51:07 -0600

    learning

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        
+        
+        Fraction fract = new Fraction();
+        Fraction fract2= new Fraction(6);
+        Fraction fract3 = new Fraction(6,7);
+        Fraction top = new Fraction();
+        top.SetTop(0);
+        top.SetBottom(0);
+        Console.WriteLine(top.GetTop());
+        Console.WriteLine(top.GetBottom());
+        Console.WriteLine(top.GetFractionString());
+        Console.WriteLine(top.GetDecimalValue());
+
     }
 }
