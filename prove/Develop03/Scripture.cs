fcdd15e0f33685f54203d46fec5efc6437553d54
casjora <casjora@gmail.com>
2024-05-11 23:56:44 -0600

    Still not able to hide them

---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,71 @@
+using System.Reflection.Metadata.Ecma335;
+using System.Xml.XPath;
+
+
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+
+    public Scripture(Reference reference,string text)
+    { //split words in text
+    /*
+    string name = "Jhon Deere";
+    string [] words = name.Split(" ");
+    Console.WriteLine(words[0]);
+    Console.WriteLine(words[1]);*/
+    _reference = reference;
+    _words=new List<Word>();
+
+    string[] words=text.Split(" ");
+    foreach (string word in words)
+    { 
+        _words.Add(new Word(word));
+    }
+
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+        //set state of randomly selected group of words to be hidden
+        // find a set of visible words
+        // Need to randomly select numberToHide
+
+        Random random = new Random();
+        int wordsHidden = 0;
+        while (wordsHidden < numberToHide)
+        {
+            int index = random.Next(_words.Count);
+            if (!_words[index].IsHidden())
+            {
+                
+                _words[index].Hide();
+                wordsHidden++;
+            }
+        }
+    }
+
+    public string GetDisplayText()
+    {
+        string displayText = $"{_reference.GetDisplayText()}\n";
+        foreach (Word word in _words)
+        {
+            displayText += word.GetDisplayText() + " ";
+        }
+        return displayText;
+    }
+
+    
+
+    public bool isCompletelyHidden()
+    {
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+
+                return false;
+        }
+    return true;
+    }
+
+}
---
+++ prove/Develop03/Word.cs
@@ -0,0 +1,32 @@
+using System.Xml.XPath;
+
+public class Word
+{
+    private string _text;
+    private bool _isHidden;
+
+    public Word(string text)
+    {
+        _text = text;
+        _isHidden = false;
+    }
+
+    public string GetDisplayText()
+    {
+        return _text;
+    }
+
+    public void Show()
+    {
+        _isHidden = false;
+    }
+    public void Hide()
+    {
+        _isHidden = true;
+    }
+
+    public bool IsHidden()
+    {
+        return _isHidden; // Check if the word is hidden
+    }
+}
