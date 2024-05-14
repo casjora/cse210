dce9b608a74e1415ac945c1c1697ff2d94db2e4a
casjora <casjora@gmail.com>
2024-05-13 20:41:35 -0600

    on my own - versiion

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment1 = new Assignment();
+        assignment1.SetStudentName("Jose");
+        assignment1.SetTopic($"Calculating cubic roots");
+        Console.WriteLine(assignment1.GetSummary());
+        MathAssignment math1 = new MathAssignment("Jose","Exponents","7.3","8-19");
+        Console.WriteLine(math1.GetHomeworkList());
+        WritingAssigment wrAssing = new WritingAssigment("Mary Waters","European History","The Causes of WW2");
+        Console.WriteLine(wrAssing.GetWritingInformation());
+
+
     }
 }
---
+++ prepare/Learning04/WritingAssignment.cs
@@ -0,0 +1,15 @@
+public class WritingAssigment : Assignment
+{
+    private string _title="";
+
+    public WritingAssigment(string studentName,string topic,string title):base(studentName,topic)
+    {
+        _title=title;
+    }
+
+    public string GetWritingInformation()
+    {
+        
+        return $"{GetStudentName()} - {GetTopic()}\n{_title} by {GetStudentName()}";
+    }
+}
