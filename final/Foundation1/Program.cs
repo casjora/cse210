a6a7415ec74f16df58dcaad0d2ed213d34e2afb4
casjora <casjora@gmail.com>
2024-06-01 13:04:49 -0600

    Program 1 completed, about to finish program 2

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -2,8 +2,25 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        var videos = new List<Video>();
+
+        var video1 = new Video("Learning C#", "John Doe", 300);
+        video1.AddComment(new Comment("Alice", "Great tutorial!"));
+        video1.AddComment(new Comment("Bob", "Very helpful, thanks!"));
+        video1.AddComment(new Comment("Charlie", "I love this!"));
+        videos.Add(video1);
+
+        var video2 = new Video("Cooking Pasta", "Jane Smith", 600);
+        video2.AddComment(new Comment("Dave", "Yummy recipe!"));
+        video2.AddComment(new Comment("Eve", "Can't wait to try this!"));
+        videos.Add(video2);
+
+        foreach (var video in videos)
+        {
+            video.Display();
+            Console.WriteLine();
+        }
     }
 }
---
+++ final/Foundation1/Video.cs
@@ -0,0 +1,34 @@
+public class Video
+{
+    private string _title;
+    private string _author;
+    private int _length;
+    private List<Comment> _comments = new List<Comment>();
+
+    public Video(string title, string author, int length)
+    {
+        _title = title;
+        _author = author;
+        _length = length;
+    }
+
+    public void AddComment(Comment comment)
+    {
+        _comments.Add(comment);
+    }
+
+    public int GetCommentCount()
+    {
+        return _comments.Count;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"{_title} by {_author} ({_length} seconds)");
+        Console.WriteLine($"Number of Comments: {GetCommentCount()}");
+        foreach (var comment in _comments)
+        {
+            Console.WriteLine($"Comment by {comment.CommenterName}: {comment.Text}");
+        }
+    }
+}
